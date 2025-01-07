50c578debaa067284a8b178bf17c438dc9ad4215
Lucas Magaldi <lucas.magaldi@hotmail.com>
2025-01-07 10:10:56 -0300

    feat (ui): create data table component

---
+++ ui/src/components/data-table/index.tsx
@@ -0,0 +1,43 @@
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+  } from "@mui/material";
+  
+  interface IPros<T extends Record<string, React.ReactNode>> {
+    columns: Array<{ key: keyof T; label: string }>
+    data: T[]
+  }
+  
+  export function DataTable<T extends Record<string, React.ReactNode>>({ columns, data }: IPros<T>) {
+    return (
+      <TableContainer component={Paper} sx={{ backgroundColor: "transparent", boxShadow: "none" }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell key={String(column.key)} sx={{ color: "#9CA3AF", fontWeight: "bold" }}>
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((row, rowIndex) => (
+                <TableRow key={rowIndex}>
+                  {columns.map((column) => (
+                    <TableCell key={String(column.key)} sx={{ color: "#E5E7EB" }}>
+                      {row[column.key]}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+      </TableContainer>
+    );
+  }
