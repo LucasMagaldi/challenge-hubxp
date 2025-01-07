46f2e31bde00fc9f3687c47b7c5db3dc61da4ae4
Lucas Magaldi <lucas.magaldi@hotmail.com>
2025-01-07 08:36:40 -0300

    refactor (api): add dotenv as provider for env variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CategoryModule } from './modules/categories/categories.module';
 import { DatabaseModule } from './database/mongoose.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    DatabaseModule,
-    CategoryModule,
-  ],
-  controllers: [],
-  providers: [],
+  imports: [DatabaseModule, CategoryModule],
 })
 export class AppModule {}
