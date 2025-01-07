44f62cd688a880c86caf101c0708bdad98cb9b83
Lucas Magaldi <lucas.magaldi@hotmail.com>
2025-01-07 06:50:37 -0300

    refac (api): access configure module globally

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CategoryModule } from './modules/categories/categories.module';
 import { DatabaseModule } from './database/mongoose.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule, CategoryModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    CategoryModule,
+  ],
   controllers: [],
   providers: [],
 })
