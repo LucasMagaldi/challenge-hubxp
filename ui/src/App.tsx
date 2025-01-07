05bb844ac3a03e02bd3e5455b61ec9a97d0ac606
Lucas Magaldi <lucas.magaldi@hotmail.com>
2025-01-07 11:21:20 -0300

    feat (ui): set up react client provider and axios instace

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,13 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./router"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { queryClient } from "./lib/react-query"
 
 function App() {
   return (
-    <RouterProvider router={router}/>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}/>
+    </QueryClientProvider>
   )
 }
 
