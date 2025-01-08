21a2283145ef40b9a82643cda6f0146d4d7ad182
Lucas Magaldi <lucas.magaldi@hotmail.com>
2025-01-07 22:47:39 -0300

    feat (ui): create product hook - handle with delete create and update

---
+++ ui/src/hooks/api-products.ts
@@ -0,0 +1,52 @@
+import { api } from "../lib/axios"
+
+interface IGetCategoriesByProduct {
+    _id: string
+    name: string
+}
+
+export interface IGetProducts {
+    _id: string;
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+    categories: IGetCategoriesByProduct[];
+}
+
+export interface IPostProduct {
+    name: string;
+    description: string;
+    price: string;
+}
+
+export async function getProducts() {
+    try {
+        const response = await api.get<IGetProducts[]>('/products')
+
+        return response.data        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function removeProduct(productId: string) {
+    try {
+        await api.delete(`/products/${productId}`)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function createProduct({ name, description, price}: IPostProduct) {
+    try {
+        await api.post('/products', {
+            name,
+            description,
+            price: parseInt(price),
+            categories: []
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
