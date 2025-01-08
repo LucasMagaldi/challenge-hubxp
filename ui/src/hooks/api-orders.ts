147211946fd9584340041cce3723adcf6f23a543
Lucas Magaldi <lucas.magaldi@hotmail.com>
2025-01-08 06:25:42 -0300

    feat (ui): create orders hooks - to create, get and remove orders

---
+++ ui/src/hooks/api-orders.ts
@@ -0,0 +1,48 @@
+import { api } from "../lib/axios"
+
+interface IGetProductsByCategories {
+    _id: string
+    name: string
+}
+
+export interface IGetOrders {
+    _id: string;
+    date: Date;
+    total: number;
+    products: IGetProductsByCategories[];
+}
+
+export interface IPostOrder {
+    date: Date;
+    total: string;
+}
+
+export async function getOrders() {
+    try {
+        const response = await api.get<IGetOrders[]>('/orders')
+
+        return response.data        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function removeOrder(orderId: string) {
+    try {
+        await api.delete(`/orders/${orderId}`)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function createOrder({ total, date }: IPostOrder) {
+    try {
+        await api.post('/orders', {
+            total,
+            date,
+            products: []
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
