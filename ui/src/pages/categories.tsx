2fa7d00a266c452a522b96a89ab96b2a30d460be
Lucas Magaldi <lucas.magaldi@hotmail.com>
2025-01-07 23:05:24 -0300

    refactor (ui): category page - use common base list component

--- ui/src/pages/categories.tsx
+++ ui/src/pages/categories.tsx
@@ -1,8 +1,6 @@
-import { Box, Button, Typography } from "@mui/material";
-import { PlusCircle } from "lucide-react";
-import { DataTable } from "../components/data-table";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getCategories, removeCategory, updateCategory } from "../hooks/api-categories";
+import { createCategory, getCategories, IGetCategories, removeCategory, updateCategory, UpdateCategoryProps } from "../hooks/api-categories";
+import { BaseList } from "../components/base-list";
 
 const columns = [
     { key: "name", label: "Category name" },
@@ -12,7 +10,7 @@ const columns = [
 
 export function Categories() {
     const queryClient = useQueryClient()
-    const { data: orders } = useQuery({
+    const { data: categories } = useQuery({
         queryKey: ['categories'],
         queryFn: () => getCategories(),
     })
@@ -20,7 +18,7 @@ export function Categories() {
     const { mutateAsync: updateCategoryFn } = useMutation({
         mutationFn: updateCategory,
         onSuccess: () => {
-            queryClient.invalidateQueries(['categories']);
+        queryClient.invalidateQueries(['categories']);
         },
     });
 
@@ -34,52 +32,21 @@ export function Categories() {
         },
     });
 
-    const handleEdit = (row: any) => {
-        updateCategoryFn(row);
-    };
+    const { mutateAsync: createCategoryFn} = useMutation({
+        mutationFn: createCategory,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['categories']);
+        }
+    })
 
-    const handleRemove = (row: any) => {
-        removeCategoryFn(row._id);
-    }
     return (
-        <Box
-            sx={{
-                backgroundColor: "rgba(31, 41, 55, 0.8)",
-                backdropFilter: "blur(10px)",
-                borderRadius: 2,
-                boxShadow: 3,
-                padding: 3,
-                border: "1px solid #334155",
-            }}
-            >
-            <Box
-                sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                marginBottom: 3,
-                }}
-            >
-                <Typography variant="h6" color="white">
-                Categories List
-                </Typography>
-                <Box sx={{ position: "relative" }}>
-                    <Button 
-                        sx={{
-                            backgroundColor:"#036316",
-                            color:"white",
-                            paddingX: 2,
-                            display:"flex",
-                            gap: 1
-                        }}
-                    > 
-                        Create 
-                        <PlusCircle />
-                    </Button>
-                </Box>
-            </Box>
-
-            <DataTable columns={columns} data={orders} onEdit={handleEdit} onRemove={handleRemove}/>
-        </Box>
+        <BaseList<IGetCategories>
+            title="Categories list"
+            columns={columns}
+            fetchData={categories}
+            editMutation={updateCategoryFn}
+            removeMutation={removeCategoryFn}
+            addMutation={createCategoryFn}
+        />
     )
 }
